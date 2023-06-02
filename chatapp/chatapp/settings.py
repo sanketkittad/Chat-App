bfa386dc636aa0d7b84763560832380bd4360ab0
sanketkittad <sanketkittad02@gmail.com>
2023-06-02 16:46:22 +0530

    init

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^6r6jc&p^^vg=39vhk@uvf(*+96dsd@f5&m43&&j6i*6kfs=m&'
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
